ce7f7b1837b32179928c7c1633fd752b22ac390c
AshishVerma777 <156294206+AshishVerma777@users.noreply.github.com>
2024-03-22 15:52:28 +0530

    change

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -118,7 +118,7 @@
                             <span class="text-success">+12.5%</span>
                         </div>
                     </div>
-                    <h3 class="mb-3">$1,42,300</h3>
+                    <h3 class="mb-3">$0</h3>
                     <div class="progress mb-2" style="height: 5px;">
                         <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -136,7 +136,7 @@
                             <span class="text-danger">-2.8%</span>
                         </div>
                     </div>
-                    <h3 class="mb-3">$8,500</h3>
+                    <h3 class="mb-3">$0</h3>
                     <div class="progress mb-2" style="height: 5px;">
                         <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
@@ -154,11 +154,11 @@
                             <span class="text-danger">-75%</span>
                         </div>
                     </div>
-                    <h3 class="mb-3">$1,12,000</h3>
+                    <h3 class="mb-3">$0</h3>
                     <div class="progress mb-2" style="height: 5px;">
                         <div class="progress-bar bg-primary" role="progressbar" style="width: 70%;" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                    <p class="mb-0">Previous Month <span class="text-muted">$1,42,000</span></p>
+                    <p class="mb-0">Previous Month <span class="text-muted">$0</span></p>
                 </div>
             </div>
         </div>
