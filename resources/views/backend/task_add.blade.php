def9c70f9a5cf4bdb098367d11b310c02bfa76a5
preetamdigital <preetam.patel@lifelinkdigital.com>
2024-04-16 22:56:21 +0530

    task issue

--- resources/views/backend/task_add.blade.php
+++ resources/views/backend/task_add.blade.php
@@ -16,8 +16,8 @@
 	<div class="col-auto float-right ml-auto">
 		<a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_task"><i class="fa fa-plus"></i> Add Task</a>
 		<div class="view-icons">
-			<a href="{{route('clients')}}" class="grid-view btn btn-link {{route_is('clients') ? 'active' : '' }}"><i class="fa fa-th"></i></a>
-			<a href="{{route('clients-list')}}" class="list-view btn btn-link {{route_is('clients-list') ? 'active' : '' }}"><i class="fa fa-bars"></i></a>
+			{{-- <a href="{{route('task')}}" class="grid-view btn btn-link {{route_is('task') ? 'active' : '' }}"><i class="fa fa-th"></i></a>
+			<a href="{{route('task-list')}}" class="list-view btn btn-link {{route_is('task-list') ? 'active' : '' }}"><i class="fa fa-bars"></i></a> --}}
 		</div>
 	</div>
 </div>
@@ -27,94 +27,41 @@
 
 
 <div class="row staff-grid-row">
-	@if (!empty($clients->count()))
-		@foreach ($clients as $client)
-			<div class="col-md-4 col-sm-6 col-12 col-lg-4 col-xl-3">
-				<div class="profile-widget">
-					<div class="profile-img">
-						<a href="javascript:void(0)" class="avatar"><img alt="" src="@if(!empty($client->avatar)) {{asset('storage/clients/'.$client->avatar)}} @else assets/img/profiles/default.jpg @endif"></a>
-					</div>
-					<div class="dropdown profile-action">
-						<a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
-					<div class="dropdown-menu dropdown-menu-right">
-						<a data-id="{{$client->id}}" data-firstname="{{$client->firstname}}" data-lastname="{{$client->lastname}}" data-email="{{$client->email}}" data-phone="{{$client->phone}}" data-avatar="{{$client->avatar}}" data-company="{{$client->company}}" class="dropdown-item editbtn" href="javascript:void(0)" data-toggle="modal"><i class="fa fa-pencil m-r-5"></i> Edit</a>
-						<a data-id="{{$client->id}}" class="dropdown-item deletebtn" href="javascript:void(0)" data-toggle="modal" ><i class="fa fa-trash-o m-r-5"></i> Delete</a>
-					</div>
-					</div>
-					<h4 class="user-name m-t-10 mb-0 text-ellipsis"><a href="javascript:void(0)">{{$client->company}}</a></h4>
-					<h5 class="user-name m-t-10 mb-0 text-ellipsis"><a href="javascript:void(0)">{{$client->firstname}} {{$client->lastname}}</a></h5>
+	<div class="col-md-12">
+		<div class="table-responsive">
+			<table class="table table-striped custom-table datatable">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>deadline</th>
+						<th>priority</th>
+						<th class="text-right no-sort">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($tasks as $task)
+					<tr>
 					
-				</div>
-			</div>
-		@endforeach
-		<x-modals.delete :route="'client.destroy'" :title="'Client'" />
-
-		<!-- Edit Client Modal -->
-		<div id="edit_client" class="modal custom-modal fade" role="dialog">
-			<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title">Edit Task</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						<form method="POST" enctype="multipart/form-data" action="{{route('client.update')}}">
-							@csrf
-							@method("PUT")
-							<div class="row">
-								<input type="hidden" id="edit_id" name="id">
-								<div class="col-md-6">
-									<div class="form-group">
-										<label class="col-form-label">First Name <span class="text-danger">*</span></label>
-										<input class="form-control edit_firstname" name="firstname" type="text">
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<label class="col-form-label">Last Name</label>
-										<input class="form-control edit_lastname" name="lastname" type="text">
-									</div>
-								</div>
-								
-								<div class="col-md-6">
-									<div class="form-group">
-										<label class="col-form-label">Email <span class="text-danger">*</span></label>
-										<input class="form-control floating edit_email" name="email" type="email">
-									</div>
-								</div>
-								
-								<div class="col-md-6">  
-									<div class="form-group">
-										<label class="col-form-label">Client Picture<span class="text-danger">*</span></label>
-										<input class="form-control floating edit_avatar" name="avatar" type="file">
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<label class="col-form-label">Phone </label>
-										<input class="form-control edit_phone" name="phone" type="text">
-									</div>
-								</div>
-								<div class="col-md-6">
-									<div class="form-group">
-										<label class="col-form-label">Company Name</label>
-										<input class="form-control edit_company" name="company" type="text">
-									</div>
+						<td>{{$task->task_name}}</td>
+						<td>{{date_format(date_create($task->task_deadline),"d M,Y")}}</td>
+						<td>{{$task->task_priority}}</td>
+						
+						<td class="text-right">
+							<div class="dropdown dropdown-action">
+								<a href="javascript:void(0)" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
+								<div class="dropdown-menu dropdown-menu-right">
+									<a data-id="{{$task->id}}" data-firstname="{{$task->task_name}}"  data-task_deadline="{{$task->task_deadline}}"  data-task_priority="{{$task->task_priority}}" class="dropdown-item editbtn" href="javascript:void(0)" data-toggle="modal"><i class="fa fa-pencil m-r-5"></i> Edit</a>
+									<a data-id="{{$task->id}}" class="dropdown-item deletebtn" href="javascript:void(0)" data-toggle="modal" ><i class="fa fa-trash-o m-r-5"></i> Delete</a>
 								</div>
 							</div>
-							
-							<div class="submit-section">
-								<button class="btn btn-primary submit-btn">Submit</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
+						</td>
+					</tr>
+					@endforeach
+					<x-modals.delete :route="'task'" :title="'Task'" />
+				</tbody>
+			</table>
 		</div>
-		<!-- /Edit Client Modal -->
-	@endif
+	</div>
 	
 </div>
 
@@ -129,48 +76,35 @@
 				</button>
 			</div>
 			<div class="modal-body">
-				<form method="POST" enctype="multipart/form-data" action="{{route('client.add')}}">
+				<form method="POST" enctype="multipart/form-data" action="{{route('task.add')}}">
 					@csrf
-					<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="col-form-label">First Name <span class="text-danger">*</span></label>
-								<input class="form-control" name="firstname" type="text">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="col-form-label">Last Name</label>
-								<input class="form-control" name="lastname" type="text">
-							</div>
-						</div>
-						
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="col-form-label">Email <span class="text-danger">*</span></label>
-								<input class="form-control floating" name="email" type="email">
-							</div>
-						</div>
-						
-						<div class="col-md-6">  
-							<div class="form-group">
-								<label class="col-form-label">Client Picture<span class="text-danger">*</span></label>
-								<input class="form-control floating" name="avatar" type="file">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="col-form-label">Phone </label>
-								<input class="form-control" name="phone" type="text">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<label class="col-form-label">Company Name</label>
-								<input class="form-control" name="company" type="text">
-							</div>
-						</div>
+					<div class="form-group">
+						<label for="task_name">Task Name:</label>
+						<input type="text" name="task_name" id="task_name" class="form-control" placeholder="Enter task name" required>
 					</div>
+					
+					<!-- Task Description -->
+				
+					<!-- Task Deadline -->
+					<div class="form-group">
+						<label for="task_deadline">Task Deadline:</label>
+						<input type="date" name="task_deadline" id="task_deadline" class="form-control" required>
+					</div>
+					
+					<!-- Task Priority -->
+					<div class="form-group">
+						<label for="task_priority">Task Priority:</label>
+						<select name="task_priority" id="task_priority" class="form-control" required>
+							<option value="low">Low</option>
+							<option value="medium">Medium</option>
+							<option value="high">High</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="task_description">Task Description:</label>
+						<textarea name="task_description" id="task_description" class="form-control" rows="3" placeholder="Enter task description"></textarea>
+					</div>
+					
 					
 					<div class="submit-section">
 						<button class="btn btn-primary submit-btn">Submit</button>
